94e8a658428fab29639b1cdde3d8d44279f51c54
priyahandekar <priya.handekar@vedantu.com>
2020-02-14 14:18:55 +0530

    component's life cycle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import LifeCycle from './LifeCycle';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App header="Props from the header..." content="Props from the content..."/>, document.getElementById('root'));
+//ReactDOM.render(<App header="Props from the header..." content="Props from the content..."/>, document.getElementById('root'));
+ReactDOM.render(<LifeCycle />, document.getElementById('root'));
+
+//Componet will unmount example, after 10 sec component will unmount.
+// setTimeout(() => {
+//     ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+// }, 10000)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
